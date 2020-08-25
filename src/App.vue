5c04b2ba1a4ec3939acfd18d1e9ccba0f62679da
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-08-25 09:10:12 +0900

    :sparkles: ヘッダーとナビゲーションの追加
    - ヘッダーコンポーネントを作成
    - ヘッダーをヘッダーコンポーネントで描画するようデザインを変更
    - Bootstrap で足りなかったCDNを追加

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="container">
-    <router-view></router-view>
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-xs-12">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import Header from './components/Header';
 
+export default {
+  components: {
+    appHeader: Header
+  }
 }
 </script>
-
-<style>
-</style>
