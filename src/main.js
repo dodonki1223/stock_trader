ee872f4232b028b7d991cf2a4615f7aaf1d12f1c
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-09-04 08:32:31 +0900

    :sparkles: フィルターを使用してファンドの見栄えを良くする
    - 金額用のグローバルなフィルターを作成する

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
 // ルーティング関連
-import VueRouter from 'vue-router'
-import { routes } from './routes'
+import VueRouter from 'vue-router';
+import { routes } from './routes';
 
 import store from './store/store';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter); 
+
+Vue.filter('currency', (value) => {
+  return '$' + value.toLocaleString();
+});
 
 const router = new VueRouter({
   routes,
   mode: 'history',
-})
+});
 
 new Vue({
   el: '#app',
   router,
   store,
   render: h => h(App)
-})
+});
